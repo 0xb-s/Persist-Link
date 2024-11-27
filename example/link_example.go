8b29f0efe5cd7e47161a141256f56405d6e6d179
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-11-26 16:30:53 -0800

    Update link_example.go

--- example/link_example.go
+++ example/link_example.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"fmt"
+	"persistlink/lib" 
+)
+
+func main() {
+
+	list := lib.Empty[int]().Append(10).Append(20).Append(30)
+	anotherList := lib.Empty[int]().Append(40).Append(50)
+	concatenatedList := list.Concat(anotherList)
+
+
+	fmt.Println("Concatenated list as slice:", concatenatedList.AsSlice()) // output is [30, 20, 10, 50, 40]
+}
